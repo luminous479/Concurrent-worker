883b205a007b352e071f7d025f83fce5e07d9fd7
luminous479 <aklemasultanakhi@gmail.com>
2026-09-11 09:45:43 +0600

    feat: add concurrent job workers with waitgroup

--- main.go
+++ main.go
@@ -1,2 +1,47 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+
+func worker(workerId int, job Job, wg *sync.WaitGroup) {
+
+	defer wg.Done()
+
+	fmt.Printf("worker %d progressing Job %d\n",workerId, job.JobId )
+
+     time.Sleep(time.Second)
+
+    fmt.Printf("worker %d finished Job %d\n",workerId, job.JobId )
+}
+
+type Job struct{
+	JobId int 
+}
+
+func main() {
+
+ var wg sync.WaitGroup
+
+  jobs := []Job {
+
+	Job{JobId: 1},
+	Job{JobId: 2},
+	Job{JobId: 3},
+	Job{JobId: 4},
+	Job{JobId: 5},
+
+  }
+
+  wg.Add(len(jobs))
+
+  for i, job := range jobs{
+	go worker( i+1 , job, &wg) 
+  }
+
+  wg.Wait()
+
+}
